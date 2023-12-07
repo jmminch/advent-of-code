61285057e6e303c9a2f5f6296bbdfed1a6e40eff
Jared Minch <jmminch+git@gmail.com>
2023-12-07 08:02:32 -0700

    2023 day 7 part 2.

--- 2023/day07/day07.pl
+++ 2023/day07/day07.pl
@@ -4,9 +4,10 @@ use Data::Dumper;
 my @hands;
 
 # Read the hands. Store the cards and the bid, and convert the cards to a
-# contiguous representation using letters a-m (m high)
+# contiguous representation using letters b-n (n high.) Letter a is reserved
+# for jokers.
 while(<>) {
-  push @hands, [ $1 =~ tr/2-9TJQKA/a-m/r, $2 ] if /^(.{5})\s+(\d+)/;
+  push @hands, [ $1 =~ tr/2-9TJQKA/b-n/r, $2 ] if /^(.{5})\s+(\d+)/;
 }
 
 # Sort the hands in ascending order
@@ -19,6 +20,23 @@ for my $i (0..$#hands) {
 }
 
 print "Part 1 result: $winnings\n";
+
+# Turn jacks ('k') into jokers ('a')
+for my $hand (@hands) {
+  $hand->[0] =~ tr/k/a/;
+}
+
+# Re-sort and calculate winnings again.
+@hands = sort handSort @hands;
+
+# Calculate the total winnings.
+$winnings = 0;
+for my $i (0..$#hands) {
+  $winnings += ($i + 1) * $hands[$i]->[1];
+}
+
+print "Part 2 result: $winnings\n";
+
 
 sub handSort {
   # Compare the rank of the hands.
@@ -38,10 +56,30 @@ sub handRank {
     $counts{$c}++;
   }
 
+  my $jokerCount = $counts{a};
+  delete $counts{a};
+
   # Now figure out how many of each count there are
   my %counts2;
   for my $count (values %counts) {
     $counts2{$count}++;
+  }
+
+  # It always is best to use jokers to match the highest count, so add them
+  # to the highest count.
+  for my $count (5, 4, 3, 2, 1, 0) {
+    # If we get to 0, then there are no non-joker cards; so this is actually
+    # a 5 of a kind hand.
+    if($count == 0) {
+      $counts2{$jokerCount}++;
+      last;
+    }
+
+    if($counts2{$count}) {
+      $counts2{$count + $jokerCount}++;
+      $counts2{$count}--;
+      last;
+    }
   }
 
   # Now return a value indicating the rank
--- 2023/day07/notes.md
+++ 2023/day07/notes.md
@@ -10,4 +10,27 @@ the order given, so that can be done with a simple alphabetical comparison
 if I do a transliteration of the hands to letters a-m, like
 `tr/2-9TJQKA/a-m/`.
 
+For part 2, we now have jokers. Jokers are the weakest card, and so I'm
+going to use a card representation of `a-n` instead with `a` being the
+joker.
 
+I believe that to determine the best hand using jokers, you just need to add
+the number of jokers to the type of card that you have the most of. I
+implemented that, but I get the wrong answer for the actual input. (Correct
+answer for the test input, though). So it seems like I've got a problem
+somewhere in my ranking logic.
+
+```
+$ perl day07.pl < input 
+Part 1 result: 248217452
+Part 2 result: 246000630
+```
+
+A little experimentation and I realized that I wasn't correctly handling the
+5-joker case. Fixing that gives the correct answer.
+
+```
+$ perl day07.pl < input 
+Part 1 result: 248217452
+Part 2 result: 245576185
+```
